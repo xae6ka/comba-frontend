82259dcd788b52d2900b3aaf081d346da5ae7cf8
xae6ka <xae6kacode@gmail.com>
2024-08-01 12:40:17 +0300

    made login/reg page

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,8 +1,29 @@
+import { Link } from 'react-router-dom';
+import css from './css/login.module.css';
+
 export default function Login() {
-    return (
-      <section>
-        <h1>Login</h1>
-      </section>
-    );
-  }
-  
+  return (
+    <section className={css.login}>
+      <form className={css.login__login}>
+        <input placeholder="login" type="text" className={css.login__input} />
+
+        <input
+          placeholder="password"
+          type="text"
+          className={css.login__input}
+        />
+
+        <button type="submit" className={css.login__btn}>
+          Reg!
+        </button>
+      </form>
+      <div className={css.login__reg}>
+        <span className={css.reg__span}>Create Account!</span>
+
+        <Link to="/reg" className={css.reg__link}>
+          Register
+        </Link>
+      </div>
+    </section>
+  );
+}
--- src/Pages/Reg/Reg.jsx
+++ src/Pages/Reg/Reg.jsx
@@ -1,8 +1,27 @@
+import { Link } from 'react-router-dom';
+import css from './css/Reg.module.css';
+
 export default function Reg() {
-    return (
-      <section>
-        <h1>Reg</h1>
-      </section>
-    );
-  }
-  
+  return (
+    <section className={css.reg}>
+      <form className={css.reg__reg}>
+        <input placeholder="login" type="text" className={css.reg__input} />
+
+        <input placeholder="e-mail" type="text" className={css.reg__input} />
+
+        <input placeholder="password" type="text" className={css.reg__input} />
+
+        <button type="submit" className={css.reg__btn}>
+          Reg!
+        </button>
+      </form>
+      <div className={css.reg__login}>
+        <span className={css.login__span}>Have Account?</span>
+
+        <Link to="/login" className={css.login__link}>
+          Login
+        </Link>
+      </div>
+    </section>
+  );
+}
